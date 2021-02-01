825dd28d12febe9d4f1f1ffb9ef59e68aab2b2f1
Eva Maria <76119852+EvaMariaCamacho@users.noreply.github.com>
2021-02-01 17:21:27 +0100

    Add files via upload

---
+++ PHP_M3/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <!-- Fuente Lato -->
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap" rel="stylesheet">
+        <!-- Iconos -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+        <!-- Hoja de estilo -->
+        <link rel="stylesheet" href="css/style.css"/>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        
+<!-- 5 bloques para ejercicio M3 -->      
+<div class="blocks">
+  <div class="blocks__block">
+    <div class="block__content">
+      <h2 class="block__title">IT</h2>            
+    </div>
+  </div>
+  <div class="blocks__block">
+    <div class="block__content">
+      <h2 class="strip__title">A</h2>      
+    </div>
+  </div>
+  <div class="blocks__block">
+    <div class="block__content">
+      <h2 class="block__title">CA</h2>        
+    </div>
+  </div>
+  <div class="blocks__block">
+    <div class="block__content">
+      <h2 class="block__title">DE</h2>      
+    </div>
+  </div>
+  <div class="blocks__block">
+    <div class="block__content">
+      <h2 class="block__title">MY <i class="fas fa-heart"></i></h2>            
+    </div>
+  </div>
+</div>
+    </body>
+</html>
